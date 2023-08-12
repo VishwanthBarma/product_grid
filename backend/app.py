6426ee912c8dafcce39180b5f40bf82f251b4de4
Barma Vishwanth <vishwanthbsavp@gmail.com>
2023-08-12 20:42:09 +0530

    integrated: python backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/recommended-products', methods=['GET'])
 def get_recommended_products():
