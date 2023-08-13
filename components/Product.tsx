e68ebb5114e68ce0ae3398ae6e0a48c4f858f1ce
Barma Vishwanth <vishwanthbsavp@gmail.com>
2023-08-13 18:47:40 +0530

    develop: product page'

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,11 +1,15 @@
+import { useRouter } from 'next/router';
 import React from 'react'
 import { AiFillStar } from "react-icons/ai";
 import { BsCurrencyRupee } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
 
 
 function Product() {
+    const router = useRouter();
+    const id = 1;
   return (
-    <div className='bg-neutral-100 p-3 rounded-xl flex flex-col space-y-1'>
+    <div onClick={() => router.push(`/product/${id}`)} className='bg-neutral-100 p-3 rounded-xl flex flex-col space-y-1 hover:shadow-lg cursor-pointer'>
         <img className="h-auto max-w-full rounded-lg mb-2" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
         <h1 className=''>Title</h1>
         <h1 className='text-xs text-neutral-400'>Description</h1>
@@ -18,7 +22,14 @@ function Product() {
             3200
         </h1>
         <h1 className='text-sm text-green-600'>In stock</h1>
-        <button className='bg-orange-400 p-1 rounded-xl font-sembold text-white'>ADD TO CART</button>
+        <div className='flex justify-between space-x-2'>
+            <button className='bg-sky-500 p-2 rounded-xl flex-1 font-sembold text-white hover:bg-sky-400 active:bg-sky-600'>
+                <h1 className='font-bold'>ADD TO CART</h1>
+            </button>
+            <button className='bg-white p-2 rounded-xl fle font-sembold text-white active:bg-sky-600 hover:bg-neutral-600'>
+                <AiOutlineHeart className='text-pink-500 text-2xl'/>
+            </button>
+        </div>
     </div>
   )
 }
