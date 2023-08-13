34422e94b6578490e64251f18587c68ed54aa013
Barma Vishwanth <vishwanthbsavp@gmail.com>
2023-08-13 19:28:53 +0530

    updated backend

---
+++ backend/algo/FinalRecommendation.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import os
+from sklearn.metrics.pairwise import cosine_similarity
+
+user_product_csv_path = os.path.join(os.getcwd(), 'gridDB', 'userProductRelation.csv')
+products_csv_path = os.path.join(os.getcwd(), 'gridDB', 'productsDB.csv')
+
+def FinalRecommendations(user_id,num_recommendations):
+    user_product_df = pd.read_csv(user_product_csv_path)
+    products_df = pd.read_csv(products_csv_path) 
+    merged_df = user_product_df.merge(products_df, on='product_id', how='left')
+    interaction_matrix = pd.pivot_table(merged_df, values=['liked_or_not', 'wishlisted_or_not', 'ordered_or_not', 'search_count'],
+                                    index='user_id', columns='product_id', fill_value=0)
+    item_similarity = cosine_similarity(interaction_matrix.T)
+    user_interactions = interaction_matrix.loc[user_id]  # Fix variable name here
+    product_scores = {}
+    for product_id in interaction_matrix.columns.get_level_values('product_id'):  # Fix variable name here
+        ordered_products = user_interactions[('ordered_or_not', product_id)]
+        same_category_products = merged_df[(merged_df['category'] == merged_df.loc[merged_df['product_id'] == product_id, 'category'].values[0]) &
+                                           (merged_df['sub_category'] == merged_df.loc[merged_df['product_id'] == product_id, 'sub_category'].values[0])]
+        if ordered_products == 0 or same_category_products['ordered_or_not'].sum() == 0:
+            score = (user_interactions[('liked_or_not', product_id)] * 3 +
+                     user_interactions[('wishlisted_or_not', product_id)] * 2 +
+                     user_interactions[('search_count', product_id)])
+            product_scores[product_id] = score
+    sorted_recommendations = sorted(product_scores.keys(), key=lambda x: product_scores[x], reverse=True)
+    return sorted_recommendations[:num_recommendations]
--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,42 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from algo.FinalRecommendation import FinalRecommendations
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route('/api/recommended-products', methods=['GET'])
-def get_recommended_products():
-    # Generate or retrieve recommended product data
-    recommended_products = [
-        {'id': 1, 'name': 'Product A'},
-        {'id': 2, 'name': 'Product B'},
-        # ...
-    ]
-    return jsonify(recommended_products)
+products_csv_path = 'gridDB/productsDB.csv'
+@app.route('/api/recommended-products/<int:user_id>', methods=['GET'])
+def get_recommended_products(user_id):
+    recommended_product_ids = FinalRecommendations(user_id, num_recommendations=100)
+    products_df = pd.read_csv(products_csv_path)
+    recommended_products = []
+    for product_id in recommended_product_ids:
+        product_info = products_df[products_df['product_id'] == product_id].iloc[0]
+        recommended_product = {
+            'id': product_id,
+            'name': product_info['product_name'],
+            'url': product_info['url'],
+            'brand': product_info['brand'],
+            'stock': product_info['stock'],
+            'f_assured': bool(product_info['f_assured']),
+            'price': product_info['price'],
+            'original_price': product_info['original_price'],
+            'discount': product_info['discount'],
+            'images': product_info['images'],
+            'seller': product_info['seller'],
+            'seller_rating': product_info['seller_rating'],
+            'return_policy': product_info['return_policy'],
+            'description': product_info['description'],
+            'specifications': product_info['specifications'],
+            'formatted_specifications': product_info['formatted_specifications'],
+            'avg_rating': product_info['avg_rating'],
+            'reviews_count': product_info['reviews_count'],
+            'category': product_info['category'],
+            'sub_category': product_info['sub_category']
+        }
+        recommended_products.append(recommended_product)
+    return recommended_products
 
 if __name__ == '__main__':
     app.run(debug=True)
