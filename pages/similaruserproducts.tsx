1686a3e7c22ef11ae0d5a61b048b9813cad31c7b
Barma Vishwanth <vishwanthbsavp@gmail.com>
2023-08-15 20:45:25 +0530

    developed: similar_user_products page

--- pages/similaruserproducts.tsx
+++ pages/similaruserproducts.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Product from '../Components/Product'
+import { ProductRecommendationContext } from '../Context/ProductRecommendationContext'
 
 function SimilaruserProducts() {
+  const {similarProducts}: any = useContext(ProductRecommendationContext);
   return (
     <div className='flex flex-col p-5 space-y-5'>
-        <h1 className='font-bold text-xl'>Similar User Products</h1>
+        <h1 className='font-bold text-xl'>Products Based On Similar Users</h1>
 
-        <div className="grid grid-cols-4 gap-4">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+        <div className="grid grid-cols-4 gap-4 place-content-center">
+
+          {
+            similarProducts?.map((product: any) => (<Product
+              key={product?.id}
+               image={product?.images[0]}
+               rating={product?.avg_rating}  
+               name={product?.name}
+               description={product?.description}
+               price={product?.price}
+               stock={product?.stock}
+               productId = {product?.id}
+                />))
+          }
+
         </div>
 
     </div>
